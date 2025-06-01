f52cc9d6beecc4e405fb2ba9a0adf4ccb1d60262
Kalama Ayubu <kalamayeng@gmail.com>
2025-06-01 06:50:07 +0300

    Moved root directory out of OneDrive, but still struggling with the server Starting... problme

--- app/templates/page.js
+++ app/templates/page.js
@@ -7,22 +7,6 @@ const TemplatesPage = () => {
       <h1 className="text-black text-center text-4xl md:text-5xl mb-10 max-w-[800px] m-auto">Choose the template that best suits your needs</h1>
       <div className="flex gap-3 sm:gap-5 md:gap-6 flex-wrap items-center justify-center mb-20">
       <TemplateCard/>
-      <TemplateCard/>
-      <TemplateCard/>
-      <TemplateCard/>
-      <TemplateCard/>
-      <TemplateCard/>
-      <TemplateCard/>
-      <TemplateCard/>
-      <TemplateCard/>
-      <TemplateCard/>
-      <TemplateCard/>
-      <TemplateCard/>
-      <TemplateCard/>
-      <TemplateCard/>
-      <TemplateCard/>
-      <TemplateCard/>
-      <TemplateCard/>
       </div>
       <Footer/>
     </div>

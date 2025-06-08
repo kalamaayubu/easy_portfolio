3d9065dd2eb79d5c1491fb1e6acfd793ff1c9308
Kalama <kalamayeng@gmail.com>
2025-06-08 20:47:56 +0300

    Refactor: Modified the loading skeleton card of the templates page

--- components/skeletons/AllTemplates.jsx
+++ components/skeletons/AllTemplates.jsx
@@ -1,7 +1,11 @@
 const AllTemplatesSkeleton = () => {
   return (
-    <div>
-      <div className="h-44 w-48 bg-gray-300 rounded-md animate-pulse mb-4" />
+    <div className="flex flex-col mb-4">
+      <div className="h-44 w-56 bg-gray-300 rounded-md animate-pulse" />
+      <div className="flex gap-2 items-center bg-red mt-2">
+        <div className="h-10 w-12 rounded-full bg-gray-300 animate-pulse"/>
+        <div className="w-full h-8 rounded-full bg-gray-300 animate-pulse"/>
+      </div>
     </div>
   );
 };
